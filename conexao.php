1e176d709793373be9cfb24248679cdad00fdd7f
Micael Miranda <micael.miranda@unidavi.edu.br>
2019-11-23 13:10:53 -0300

    Revert "corrigido o erro de conexao com o banco de dados"
    This reverts commit 575526d8405e43d6727fba6d1a3643fb6490b281.

--- conexao.php
+++ conexao.php
@@ -1,10 +1,10 @@
 <?php
 
-define('localhost',' 127.0.0.1');
-define('nome', 'root');
+define('host',' 127.0.0.1');
+define('usuario', 'root');
 define('senha', '123');
 define('db','trabalho');
 
-$conexao = @mysqli_connect('localhost', 'nome', 'senha', 'db');
+$conexao = mysqli_connect(host, usuario, senha, db);
 
 ?>
