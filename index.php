852615fc31fb2434b847a8865d6faff8b9ddc46c
gugaluck <53919975+gugaluck@users.noreply.github.com>
2019-11-24 13:55:27 -0300

    Update index.php
    feito o inicio do index.php, onde coloquei os campos para o login

--- index.php
+++ index.php
@@ -5,5 +5,23 @@
     <title>Trabalho PHP</title>
     <link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
+<body>
+		<h2>Login</h2>
+		<?php
+			if(isset($_SESSION['msg'])){
+				echo $_SESSION['msg'];
+				unset($_SESSION['msg']);
+			}
+		?>
+		<form method="POST" action="valida.php">
+			<label>Usuário</label>
+			<input type="text" name="usuario" placeholder="Digite seu usuário"><br><br>
+			
+			<label>Senha</label>
+			<input type="password" name="senha" placeholder="Digite sua senha"><br><br>
+			
+			<input type="submit" name="btnLogin" value="Acessar">	
+		</form>
+</body>
 
 </html>
