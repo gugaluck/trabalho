4c71e35b6f533ee93d8d9f73a2c8d6661d9aba2d
gugaluck <53919975+gugaluck@users.noreply.github.com>
2019-11-24 14:36:09 -0300

    alterei a conexão com o banco northwind

--- conexao.php
+++ conexao.php
@@ -2,8 +2,8 @@
 
 define('localhost',' 127.0.0.1');
 define('nome', 'root');
-define('senha', '123');
-define('db','trabalho');
+define('senha', '');
+define('db','northwind');
 
 $conexao = @mysqli_connect('localhost', 'nome', 'senha', 'db');
 
