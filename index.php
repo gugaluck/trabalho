1a100b9e6c915451fda4b216e54082d1216b81d3
gugaluck <53919975+gugaluck@users.noreply.github.com>
2019-11-25 09:23:19 -0300

    realizado umas melhorias do index

--- index.php
+++ index.php
@@ -3,25 +3,25 @@
 <head>
     <meta charset="utf-8">
     <title>Trabalho PHP</title>
-    <link rel="stylesheet" type="text/css" href="css/login.css">
+	<link rel="stylesheet" type="text/css" href="index.css"> 
 </head>
 <body>
-		<h2>Login</h2>
-		<?php
-			if(isset($_SESSION['msg'])){
-				echo $_SESSION['msg'];
-				unset($_SESSION['msg']);
-			}
-		?>
-		<form method="POST" action="valida.php">
-			<label>Usuário</label>
-			<input type="text" name="usuario" placeholder="Digite seu usuário"><br><br>
-			
-			<label>Senha</label>
-			<input type="password" name="senha" placeholder="Digite sua senha"><br><br>
-			
-			<input type="submit" name="btnLogin" value="Acessar">	
-		</form>
+	<?php
+		if(isset($_SESSION['msg'])){
+			echo $_SESSION['msg'];
+			unset($_SESSION['msg']);
+		}
+	?>
+	<div class="login">
+		<form class="form-logar">       
+			<h2 class="form-logar-heading">Login</h2>
+			<input type="text" class="form-campo" name="nome" placeholder="Digite seu usuário" required="" autofocus="" />
+			<input type="password" class="form-campo" name="senha" placeholder="Digite sua senha" required=""/>      
+			<label class="checkbox">
+			</label>
+			<button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>   
+    	</form>
+	</div>
 </body>
 
 
