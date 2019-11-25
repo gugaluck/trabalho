904d7a415ea48ebd5c6bf0a207c8a640d43bda7c
gugaluck <53919975+gugaluck@users.noreply.github.com>
2019-11-25 10:34:14 -0300

    trocando de usuario e senha para nome e sobrenome

--- valida.php
+++ valida.php
@@ -2,22 +2,19 @@
 session_start();
 include('conexao.php');
  
-if(empty($_POST['usuario']) || empty($_POST['senha'])) {
+if(empty($_POST['nome']) || empty($_POST['sobrenome'])) {
 	header('Location: index.php');
 	exit();
 }
  
-$usuario = mysqli_real_escape_string($conexao, $_POST['usuario']);
-$senha = mysqli_real_escape_string($conexao, $_POST['senha']);
- 
-$query = "select usuario from usuario where usuario = '{$usuario}' and senha = md5('{$senha}')";
- 
+$usuario = mysqli_real_escape_string($conexao, $_POST['nome']);
+$senha = mysqli_real_escape_string($conexao, $_POST['sobrenome']);
+$query = "select * from funcionarios where nome = '{$nome}' and sobrenome {$sobrenome}";
 $result = mysqli_query($conexao, $query);
- 
 $row = mysqli_num_rows($result);
-//verifica o login 
+ 
 if($row == 1) {
-	$_SESSION['usuario'] = $usuario;
+	$_SESSION['nome'] = $nome;
 	header('Location: painel.php');
 	exit();
 
