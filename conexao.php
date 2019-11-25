4c7da02c2201076d7d87a13ea2278713b31afa9e
gugaluck <53919975+gugaluck@users.noreply.github.com>
2019-11-25 09:23:56 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -5,6 +5,9 @@ define('nome', 'root');
 define('senha', '');
 define('db','northwind');
 
+ini_set('display_errors', true);
+error_reporting(E_ALL);
+
 $conexao = @mysqli_connect('localhost', 'nome', 'senha', 'db');
 
 ?>
