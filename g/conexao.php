d928a104b94727f7a2969769369b5e5c5c120ad6
Micael Miranda <micael.miranda@unidavi.edu.br>
2019-11-25 19:06:14 -0300

    corrigido o erro de acesso com o banco de dados

---
+++ g/conexao.php
@@ -0,0 +1,12 @@
+<?php
+$username = 'root';
+$password = '';
+$banco = 'aula';
+
+try {
+    $conn = new PDO('mysql:host=localhost;dbname='.$banco, 
+    $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo 'ERROR: '. $e->getMessage();
+}   
