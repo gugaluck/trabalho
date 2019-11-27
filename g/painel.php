b9ac685ba3aa6c48cba01b3411cfbe017fbfb7d5
gugaluck <gustavoluckmann@live.com>
2019-11-27 14:29:25 -0300

    painel ainda não retorna após login sucessful

--- g/painel.php
+++ g/painel.php
@@ -1,8 +1,30 @@
-<?php
+<html>
+<head>
+
+<?php 
 session_start();
-if(!empty($_SESSION['id'])){
-	echo "teste";
-}else{
-	$_SESSION['msg'] = "teste";
-	header("Location: valida.php");	
-}
+if((!isset ($_SESSION['login']) == true) and (!isset ($_SESSION['senha']) == true))
+{
+  unset($_SESSION['login']);
+  unset($_SESSION['senha']);
+  header('location:index.php');
+  }
+ 
+$logado = $_SESSION['login'];
+?>
+
+<title>Trabalho</title>
+</head>
+ 
+<body>
+	<table width="600" height="400">
+		<tr>
+			<td height="60" colspan="1"> Teste
+				<?php
+					echo" test $logado";
+				?>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
