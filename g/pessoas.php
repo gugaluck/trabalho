6a63b6d535fd4e563eb56f31def239149ebc78fb
Micael Miranda <micael.miranda@unidavi.edu.br>
2019-11-26 22:47:50 -0300

    retornando os dados dos fornecedores do banco de dados

--- g/pessoas.php
+++ g/pessoas.php
@@ -1,20 +1,45 @@
 <?php
+echo "<table style='border: solid 1px black;'>";
+echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
 
-    include ('conexao.php');
+class TableRows extends RecursiveIteratorIterator {
+    function __construct($it) {
+        parent::__construct($it, self::LEAVES_ONLY);
+    }
 
-    if (isset($_GET['id']))
-        $id = $_GET['id'];
+    function current() {
+        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    }
 
-    try {
-        if (isset($id)) {
-            $stmt = $conn->prepare('SELECT * FROM clientes WHERE id = :IDCliente');
-            $stmt->bindParam(':IDCliente', $id, PDO::PARAM_INT);
-        } else {
-            $stmt = $conn->prepare('SELECT * FROM clientes  ');
-        }
+    function beginChildren() {
+        echo "<tr>";
+    }
 
-        $stmt->execute();
-    
+    function endChildren() {
+        echo "</tr>" . "\n";
+    }
+}
 
-        $result = $stmt->fetchAll();
+$username = 'root';
+$password = '';
+$banco = 'trabalho';
+
+try {
+    $conn = new PDO('mysql:host=localhost;dbname='.$banco, 
+    $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $stmt = $conn->prepare("SELECT * FROM fornecedores");
+    $stmt->execute();
+
+    // set the resulting array to associative
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+        echo $v;
+    }
+}
+catch(PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+$conn = null;
+echo "</table>";
 ?>
