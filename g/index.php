a5049bafd894f2eaa0269e21231d2c6cf3c82211
gugaluck <gustavoluckmann@live.com>
2019-11-27 14:29:37 -0300

    tentando com login e senha, criei tabela no banco apenas para teste

--- g/index.php
+++ g/index.php
@@ -15,8 +15,8 @@
 	<div class="login">
 		<form class="form-logar" method="POST" action="valida.php">       
 			<h2 class="form-logar-heading">Login</h2>
-			<input type="text" class="form-campo" name="nome" placeholder="Digite seu nome" required="" autofocus="" />
-			<input type="text" class="form-campo" name="sobrenome" placeholder="Digite seu sobrenome" required=""/>      
+			<input type="text" class="form-campo" name="login" placeholder="Digite seu nome" required="" autofocus="" />
+			<input type="text" class="form-campo" name="senha" placeholder="Digite seu sobrenome" required=""/>      
 			<label class="checkbox">
 			</label>
 			<button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>   
