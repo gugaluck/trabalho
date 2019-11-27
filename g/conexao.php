1ee25563306b3a019d5f3039686505597c5e3c18
Micael Miranda <micael.miranda@unidavi.edu.br>
2019-11-26 21:56:08 -0300

    conexao.php funcionando corretamento

--- g/conexao.php
+++ g/conexao.php
@@ -1,19 +1,12 @@
 <?php
-
-function getConnection(){
 $username = 'root';
 $password = '';
 $banco = 'trabalho';
 
 try {
-    $conn = new PDO('mysql:host=mysql;dbname='.$banco, 
+    $conn = new PDO('mysql:host=localhost;dbname='.$banco, 
     $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
-    echo 'ERROR: '. $e->getMessage();
-}   
-
-$teste = getConnection();
-print_r($teste);
-
+    echo 'ERROR: ' . $e->getMessage();
 }
