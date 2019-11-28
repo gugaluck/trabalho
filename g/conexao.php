dff9c048b17fc769f3f8b0c6c0d9879214729bf9
gugaluck <gustavoluckmann@live.com>
2019-11-28 15:52:05 -0300

    Update conexao.php

--- g/conexao.php
+++ g/conexao.php
@@ -1,11 +1,10 @@
 <?php
 $username = 'root';
 $password = '';
-$banco = 'trabalho';
+$banco = 'northwind';
 
 try {
-    $conn = new PDO('mysql:host=localhost;dbname='.$banco, 
-    $username, $password);
+    $conn = new PDO('mysql:host=localhost;dbname='.$banco, $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo 'ERROR: ' . $e->getMessage();
