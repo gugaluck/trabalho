792dea63854a89c211165fc27530c02e4856beff
gugaluck <gustavoluckmann@live.com>
2019-11-28 15:51:49 -0300

    ajustes da validação do login

--- g/valida.php
+++ g/valida.php
@@ -8,23 +8,20 @@ $senha = $_POST['senha'];
 $sql = ("SELECT * FROM usuario WHERE login='$login' AND senha='$senha'") or die("login ou senha incorretos");
 $pdo = new PDO("mysql:host=localhost;dbname=northwind", "root", ""); 
 
+$_SESSION['logado'] = '0';
+
 if($stmt = $pdo->prepare($sql)){
   $stmt->bindParam(':utilizador', $param_username, PDO::PARAM_STR);
 
-  if($stmt->execute()){
-      if($stmt->rowCount() == 1){
-          if($row = $stmt->fetch()){
-              if(password_verify($senha, $row)){          
-                  session_start();
-                  $_SESSION['utilizador'] = $login;      
-                  header("location: painel.php");
-              }
-            }
-          }
-        }
+  if($stmt->execute())
+      if($stmt->rowCount() == 1)
+          if($row = $stmt->fetch())
+                $_SESSION['logado'] = '1';
+              
+            
+          
+        
       }
-  else{
-    echo("erro");
-  }
+      header('location: painel.php');
           
 ?>
