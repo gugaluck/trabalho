98b81510a388884f8fb850af4b49100d68b23862
gugaluck <gustavoluckmann@live.com>
2019-11-29 11:12:52 -0300

    arrumado os bug

--- g/valida.php
+++ g/valida.php
@@ -4,11 +4,7 @@ include('conexao.php');
 
 
 $login = $_POST['login'];
-<<<<<<< HEAD
-$senha = md5($_POST['senha']);
-=======
 $senha = $_POST['senha'];
->>>>>>> master
 $sql = ("SELECT * FROM usuario WHERE login='$login' AND senha='$senha'") or die("login ou senha incorretos");
 $pdo = new PDO("mysql:host=localhost;dbname=northwind", "root", ""); 
 
@@ -21,12 +17,6 @@ if($stmt = $pdo->prepare($sql)){
       if($stmt->rowCount() == 1)
           if($row = $stmt->fetch())
                 $_SESSION['logado'] = '1';
-<<<<<<< HEAD
-      } 
-      header('location: painel.php');          
-?>
-=======
       }
-      header('location: painel.php');          
+      header('location: menu.php');          
 ?>
->>>>>>> master
