84b656b0b42067b310ab3e0afb4fe25398d58ba9
gugaluck <gustavoluckmann@live.com>
2019-11-29 13:22:54 -0300

    implementando código pro css

--- g/menu.php
+++ g/menu.php
@@ -1,22 +1,29 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Menu</title>
+	<link rel="stylesheet" type="text/css" href="menu.css"> 
+</head>
+
 <nav>
-<?php
+<table>
+    <?php
 
-session_start();
-if  (($_SESSION['logado']) == '0'){
-	$_SESSION['logado'] = '2';
-	  header('location:index.php');
-}
- 
+    session_start();
+    if  (($_SESSION['logado']) == '0'){
+        $_SESSION['logado'] = '2';
+        header('location:index.php');
+    }
+        $menu = ['CLIENTES', 'FORNECEDORES', 'PESSOAS', 'TRANSPORTADORA', 'REGIAO', 'PRODUTOS'];
+        $links = ['clientes.php','fornecedores.php','pessoas.php','transportadora.php','regiao.php','produtos.php'];
+        print "<ul>";
 
-    $menu = ['CLIENTES', 'FORNECEDORES', 'PESSOAS', 'TRANSPORTADORA', 'REGIAO', 'PRODUTOS'];
-    $links = ['clientes.php','fornecedores.php','pessoas.php','transportadora.php','regiao.php','produtos.php'];
-
-    print "<ul>";
-
-    for($c=0;$c<6;$c++)
-    print "<a href='$links[$c]'><li>$menu[$c]</li></a>"
+        for($c=0;$c<6;$c++)
+        print "<a href='$links[$c]'><li>$menu[$c]</li></a>"
 
 
-   
-?>
+    
+    ?>
+</table>
 </nav>
+</html>
