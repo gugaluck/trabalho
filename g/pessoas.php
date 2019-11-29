7b9f7b082486f27e61c657510f9b58d73684a32e
gugaluck <gustavoluckmann@live.com>
2019-11-29 13:22:46 -0300

    implementando código pro css

--- g/pessoas.php
+++ g/pessoas.php
@@ -1,45 +1,56 @@
-<?php
-echo "<table style='border: solid 1px black;'>";
-echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Menu</title>
+        <link rel="stylesheet" type="text/css" href="menu.css"> 
+    </head>
+    <button>
+        <a href="menu.php">Voltar para o menu</a>
+    </button>    
 
-class TableRows extends RecursiveIteratorIterator {
-    function __construct($it) {
-        parent::__construct($it, self::LEAVES_ONLY);
+    <?php
+    echo "<table style='border: solid 1px black;'>";
+    echo "<tr><th>Id</th><th>Firstname</th><th>Lastname</th></tr>";
+
+    class TableRows extends RecursiveIteratorIterator {
+        function __construct($it) {
+            parent::__construct($it, self::LEAVES_ONLY);
+        }
+
+        function current() {
+            return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+        }
+
+        function beginChildren() {
+            echo "<tr>";
+        }
+
+        function endChildren() {
+            echo "</tr>" . "\n";
+        }
     }
 
-    function current() {
-        return "<td style='width:150px;border:1px solid black;'>" . parent::current(). "</td>";
+    $username = 'root';
+    $password = '';
+    $banco = 'northwind';
+
+    try {
+        $conn = new PDO('mysql:host=localhost;dbname='.$banco, 
+        $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $stmt = $conn->prepare("SELECT * FROM fornecedores");
+        $stmt->execute();
+
+        // set the resulting array to associative
+        $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
+            echo $v;
+        }
     }
-
-    function beginChildren() {
-        echo "<tr>";
+    catch(PDOException $e) {
+        echo "Error: " . $e->getMessage();
     }
-
-    function endChildren() {
-        echo "</tr>" . "\n";
-    }
-}
-
-$username = 'root';
-$password = '';
-$banco = 'northwind';
-
-try {
-    $conn = new PDO('mysql:host=localhost;dbname='.$banco, 
-    $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $conn->prepare("SELECT * FROM fornecedores");
-    $stmt->execute();
-
-    // set the resulting array to associative
-    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
-    foreach(new TableRows(new RecursiveArrayIterator($stmt->fetchAll())) as $k=>$v) {
-        echo $v;
-    }
-}
-catch(PDOException $e) {
-    echo "Error: " . $e->getMessage();
-}
-$conn = null;
-echo "</table>";
-?>
+    $conn = null;
+    echo "</table>";
+    ?>
+</html>
