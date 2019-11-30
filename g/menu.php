e47680ba4d6e220d5b5befffe94136da25e15e85
Micael Miranda <micael.miranda@unidavi.edu.br>
2019-11-30 14:21:40 -0300

    feito a alteração do nome pessoas para territorio

--- g/menu.php
+++ g/menu.php
@@ -14,8 +14,8 @@
         $_SESSION['logado'] = '2';
         header('location:index.php');
     }
-        $menu = ['CLIENTES', 'FORNECEDORES', 'PESSOAS', 'TRANSPORTADORA', 'REGIAO', 'PRODUTOS'];
-        $links = ['clientes.php','fornecedores.php','pessoas.php','transportadora.php','regiao.php','produtos.php'];
+        $menu = ['CLIENTES', 'FORNECEDORES', 'TERRITORIO', 'TRANSPORTADORA', 'REGIAO', 'PRODUTOS'];
+        $links = ['clientes.php','fornecedores.php','territorio.php','transportadora.php','regiao.php','produtos.php'];
         print "<ul>";
 
         for($c=0;$c<6;$c++)
